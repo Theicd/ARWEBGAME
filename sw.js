978acf99dfbf6ffb7a99f0e22ab3374f92b3288c
Theicd <129458154+Theicd@users.noreply.github.com>
2026-01-15 15:16:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,26 @@
-const CACHE_NAME = 'iron-ar-v81-webxr-ar';
-const urlsToCache = [
+const CACHE_NAME = 'iron-ar-v84-pwa-full';
+const PRECACHE_URLS = [
   './',
   './index.html',
+  './menu.css',
+  './sensors.html',
+  './functions.html',
+  './game.html',
   './style_v9.css',
   './game_logic_v10.js',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon.svg'
 ];
 
+// Install - cache all static assets
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Force activation immediately
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('[SW] Caching app shell');
+        return cache.addAll(PRECACHE_URLS);
+      })
   );
 });
 
